e3716547da18ab2df25c77043feaa7f3bd9ebf8b
jkrishnarao2003 <jkrishnarao2003@gmail.com>
2024-03-02 01:09:54 -0600

    web mvc config

--- src/main/java/com/test/spring/web/WebConfig.java
+++ src/main/java/com/test/spring/web/WebConfig.java
@@ -1,8 +1,14 @@
 package com.test.spring.web;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,8 +16,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+	
+	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
 		System.out.println("WebConfig.addViewControllers()");
-       // registry.addViewController("/").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+        registry.addViewController("/error").setViewName("error");
+    }
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		System.out.println("WebConfig.configureViewResolvers()");
+		registry.jsp("/WEB-INF/view/", ".html");
+	}
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/public", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
     }
 }
